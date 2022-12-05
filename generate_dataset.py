d73cec0871dd6a2a593e6d9579ab90adbd13db50
Oliver Guhr <oliver.guhr@posteo.de>
2022-12-05 16:11:44 +0100

    fixes bugs with generateting the training data

--- generate_dataset.py
+++ generate_dataset.py
@@ -6,8 +6,8 @@ import os
 from transformers import AutoTokenizer
 from tqdm import tqdm
 
-def tokenizer_check_if_text_too_long(text, tokenizer):
-    data = tokenizer.batch_encode_plus([text],max_length=1024,truncation=True,return_overflowing_tokens=True )    
+def tokenizer_check_if_text_too_long(text, tokenizer, max_length):
+    data = tokenizer.batch_encode_plus([text],max_length=max_length,truncation=True,return_overflowing_tokens=True )    
     if len(data["input_ids"]) > 1:
         return True
     else:
@@ -16,7 +16,7 @@ def tokenizer_check_if_text_too_long(text, tokenizer):
 def delete_characters(text, char_delete_percentage=0.01):
     modifyed_line = []   
     for char in text:
-        if random.random() > char_delete_percentage and char not in digits:
+        if random.random() > char_delete_percentage or char in digits:
             modifyed_line.append(char)
     return "".join(modifyed_line)
 
@@ -37,7 +37,7 @@ def replace_characters(text, augmentation_probability=0.01):
             modifyed_line.append(char)
     return "".join(modifyed_line)
 
-def swap_characters_case(text, augmentation_probability=0.05):
+def swap_characters_case(text, augmentation_probability=0.005):
     modifyed_line = []   
     for char in text:
         if random.random() <= augmentation_probability:            
@@ -45,9 +45,20 @@ def swap_characters_case(text, augmentation_probability=0.05):
         modifyed_line.append(char)
     return "".join(modifyed_line)
 
+def lower_case_words(text, augmentation_probability=0.5):
+    modifyed_line = []   
+    for word in text.split():
+        if word[0].islower() == False and random.random() <= augmentation_probability:            
+            word = word.lower()
+        modifyed_line.append(word)
+    return " ".join(modifyed_line)
+
+
 clean_chars = re.compile(r'[^A-Za-zöäüÖÄÜß,.!?’\'$%€0-9\(\)\- ]', re.MULTILINE)
 def cleanup(text):    
     text = clean_chars.sub('', text)
+    #print("bug: somehow all numbers are removed - this is might be due to this regex")
+    #exit()
     #text = text.replace("\n", "")
     #text = text.replace('"','\\"')
     return text
@@ -65,9 +76,12 @@ def combine_sentences(text, sentences, augmentation_probability = 1):
     else:
         return text
 
-def delete_word(text, augmentation_probability = 0.001):
+def delete_word(text, augmentation_probability = 0.001):        
     if random.random() < augmentation_probability:
         words = text.split()
+        if len(words) < 3:
+            # do not delete word in short text, as there will be no context to guess the word
+            return text
         word_to_remove = random.randint(0,len(words)-1)
         words.pop(word_to_remove)
         return " ".join(words)
@@ -80,33 +94,40 @@ if __name__ == "__main__":
     language = "en"
     num_lines = sum(1 for line in open(data_file,'r'))
 
-    with open(data_file,'r') as file:
-        sentences = file.readlines(int(num_lines*0.5))
-        sentences = [cleanup(sentence) for sentence in sentences]
+    #with open(data_file,'r') as file:
+    #    sentences = file.readlines(int(num_lines*0.5))
+    #    sentences = [cleanup(sentence) for sentence in sentences]
     
     tokenizer = AutoTokenizer.from_pretrained("facebook/bart-base")
     with open(language+".csv","w",encoding='utf-8') as output:        
         with open(data_file,'r') as file:
             for line in tqdm(file, total=num_lines):
                 line = cleanup(line)
-                line = combine_sentences(line,sentences)                
-                if tokenizer_check_if_text_too_long(line,tokenizer):
-                    print("text too long ({len(line)}):\n"+line)
+                if len(line) < 1:
+                    continue 
+                #line = combine_sentences(line,sentences)                
+                if tokenizer_check_if_text_too_long(line,tokenizer,max_length=1024):
+                    print(f"skipping line as its too long ({len(line)}):\n"+line)
+                    continue
                 
-                if random.random() >0.1:
-                    # we will leave 10% of the data untouched, to teach the 
+                if random.random() >0.02:
+                    # we will leave 2% of the data untouched, to teach the 
                     # model, not to "overact" on the texts
                     new_line = delete_word(line)
                     new_line = delete_characters(new_line)
                     new_line = insert_characters(new_line)
                     new_line = replace_characters(new_line)
-                    new_line = swap_characters_case(new_line)                                
+                    new_line = swap_characters_case(new_line)         
+                    new_line = lower_case_words(new_line)                                           
                     new_line = remove_punctuation(new_line)
+                else:
+                    new_line = line
                 output.write(f'"{new_line.strip()}","{line.strip()}"\n')        
     os.system(f"echo \"text,summary\" > {language}.train.csv")
-    os.system(f"head -n 299000 {language}.csv >> {language}.train.csv")
+    num_lines = sum(1 for line in open(f"{language}.csv",'r'))
+    os.system(f"head -n {num_lines-2000} {language}.csv >> {language}.train.csv")
     os.system(f"echo \"text,summary\" > {language}.test.csv")
-    os.system(f"tail -n 1000 {language}.csv >> {language}.test.csv")
+    os.system(f"tail -n 2000 {language}.csv >> {language}.test.csv")
 
 
 
